9287bfd2a972dfc87d25eeef78fe64c0d4d7a18c
blackwaltzone <dustin.heyden@gmail.com>
2016-02-23 10:08:05 -0600

    Update Entity.lua

--- Entity.lua
+++ Entity.lua
@@ -3,8 +3,24 @@ Entity.__index = Entity
 
 
 function Entity:Create(def)
-	local this = 
-	{
-		mSprite = Sprite.Creat()
-		mTexture = Texture.Find(def.Texture)
-	}
+    local this =
+    {
+        mSprite = Sprite.Create(),
+        mTexture = Texture.Find(def.texture),
+        mHeight = def.height,
+        mWidth = def.width,
+        mTileX = def.tileX,
+        mTileY = def.tileY,
+        mStartFrame = def.startFrame,
+    }
+
+    this.mSprite:SetTexture(this.mTexture)
+    this.mUVs = GenerateUVs(this.mWidth, this.mHeight, this.mTexture)
+    setmetatable(this, self)
+    this:SetFrame(this.mStartFrame)
+    return this
+end
+
+function Entity:SetFrame(frame)
+    self.mSprite:SetUVs(unpack(self.mUVs[frame]))
+end
