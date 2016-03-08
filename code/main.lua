85d926c4648ab03e731461411cd78f4964c2b224
blackwaltzone <dustin.heyden@gmail.com>
2016-03-08 07:51:47 -0800

    Added files via upload

--- code/main.lua
+++ code/main.lua
@@ -15,52 +15,59 @@ Asset.Run('Dependencies.lua')
 
 gRenderer = Renderer.Create()
 
-local mapDef = CreateMap()
-mapDef.on_wake = {}
-mapDef.actions = {}
-mapDef.trigger_types = {}
-mapDef.triggers = {}
+--local mapDef = CreateMap()
+--mapDef.on_wake = {}
+--mapDef.actions = {}
+--mapDef.trigger_types = {}
+--mapDef.triggers = {}
 
 -- 11, 3, 1 == x, y, layer
 
-
---local CreateBlock = function(stack)
---	return
---	{
---		Enter = function() end,
---		Exit = function() end, 
---		HandleInput = function(self)
---			stack:Pop()
---		end,
---		Render = function() end,
---		Update = function(self)
---			return false
---		end
---	}
---end
-
 local stack = StateStack:Create()
-local explore = ExploreState:Create(stack, mapDef, Vector.Create(11, 3, 1))
-local menu = InGameMenuState:Create(stack)
 
-gWorld = World:Create()
-gIcons = Icons:Create(Texture.Find("inventory_icons.png"))
-stack:Push(explore)
-stack:Push(menu)
+-- ******** For the menu/explore state ********
+--local explore = ExploreState:Create(stack, mapDef, Vector.Create(11, 3, 1))
+--local menu = InGameMenuState:Create(stack)
 
-
---stack:Push(FadeState:Create(stack))
---stack:Push(CreateBlock(stack))
---stack:PushFit(gRenderer, 0, 0, "Where am I?")
---stack:Push(CreateBlock(stack))
---stack:PushFit(gRenderer, -50, 50, "My head hurts!")
---stack:Push(CreateBlock(stack))
---stack:PushFit(gRenderer, -100, 100, "Uh...")
+-- ******** For the menu stuff ********
+--gWorld = World:Create()
+--gIcons = Icons:Create(Texture.Find("inventory_icons.png"))
+--stack:Push(explore)
+--stack:Push(menu)
+local intro = 
+{
+	Scene
+	{
+		map = "player_house",
+		focusX = 14,
+		focusY = 20,
+		hideHero = true,
+	},
+	BlackScreen(),
+	Play("rain"),
+	NoBlock(
+		FadeSound("rain", 0, 1, 3)
+	),	-- 0 -> 1 in 3 sec
+	Caption("place", "title", "Village of Sontos"),
+	Caption("time", "subtitle", "MIDNIGHT"),
+	Wait(2),
+	--NoBlock(FadeOutCaption("place", 3)),
+	FadeOutCaption("place", 3),
+	FadeOutCaption("time", 3),
+	FadeSound("rain", 1, 0, 1),		-- 1 -> 0 in 1 sec
+	KillState("place"),
+	KillState("time"),
+	FadeOutScreen(),
+	Wait(2),
+	Stop("rain"),
+}
+local storyboard = Storyboard:Create(stack, intro)
+stack:Push(storyboard)
 
 
 function update()
     local dt = GetDeltaTime()
     stack:Update(dt)
     stack:Render(gRenderer)
-    gWorld:Update(dt)
+    --gWorld:Update(dt)
 end
